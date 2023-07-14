cbc515932d4a45f77771bd6a945fbaec7eb5004d
YohanesMarchel <aclprcjt@gmail.com>
2023-07-14 14:56:02 +0800

    float and image group

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const body = document.querySelector('html');
-const btnDarkMode = document.querySelector('#toggle');
+const body = document.querySelector("html");
+const btnDarkMode = document.querySelector("#toggle");
 
-btnDarkMode.addEventListener('click', ()=>{
-    body.classList.toggle('dark')
-})
+btnDarkMode.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+
+const btnScrollUp = document.querySelector(".scrollup");
+const icon = document.querySelector(".scrollup div i");
+
+btnScrollUp.addEventListener("click", () => {
+  icon.classList.add("animate-scrollup");
+
+  const delet = () => {
+    icon.classList.remove("animate-scrollup");
+  };
+
+  setTimeout(delet, 1500);
+});
